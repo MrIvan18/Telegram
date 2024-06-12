87aef829a1f6d1c20f1f75e5bdeae0f4dc00ee4a
Joannis <109157353+MrIvan18@users.noreply.github.com>
2024-06-12 17:25:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,13 @@ let btn6 = document.getElementById("btn6")
 btn1.addEventListener("click", function(){
     if(tg.MainButton.isVisible && item == "1") {
         tg.MainButton.hide();
+        tg.MainButton.color = "#d4a066"; // Сброс цвета кнопки
         item = "";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 1!")
         item = "1";
+        tg.MainButton.color = "#2cab37"; // Устанавливаем цвет кнопки
         tg.MainButton.show();
     }
 });
@@ -29,55 +31,69 @@ btn1.addEventListener("click", function(){
 btn2.addEventListener("click", function(){
     if(tg.MainButton.isVisible && item == "2") {
         tg.MainButton.hide();
+        tg.MainButton.color = "#d4a066"; // Сброс цвета кнопки
         item = "";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 2!")
         item = "2";
+        tg.MainButton.color = "#2cab37"; // Устанавливаем цвет кнопки
         tg.MainButton.show();
     }
 });
 
 btn3.addEventListener("click", function(){
-    if(tg.MainButton.isVisible) {
+    if(tg.MainButton.isVisible && item == "3") {
         tg.MainButton.hide();
+        tg.MainButton.color = "#d4a066"; // Сброс цвета кнопки
+        item = "";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 3!")
         item = "3";
+        tg.MainButton.color = "#2cab37"; // Устанавливаем цвет кнопки
         tg.MainButton.show();
     }
 });
 
 btn4.addEventListener("click", function(){
-    if(tg.MainButton.isVisible) {
+    if(tg.MainButton.isVisible && item == "4") {
         tg.MainButton.hide();
+        tg.MainButton.color = "#d4a066"; // Сброс цвета кнопки
+        item = "";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 4!")
         item = "4";
+        tg.MainButton.color = "#2cab37"; // Устанавливаем цвет кнопки
         tg.MainButton.show();
     }
 });
 
 btn5.addEventListener("click", function(){
-    if(tg.MainButton.isVisible) {
+    if(tg.MainButton.isVisible && item == "5") {
         tg.MainButton.hide();
+        tg.MainButton.color = "#d4a066"; // Сброс цвета кнопки
+        item = "";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 5!")
         item = "5";
+        tg.MainButton.color = "#2cab37"; // Устанавливаем цвет кнопки
         tg.MainButton.show();
     }
 });
 
 btn6.addEventListener("click", function(){
-    if(tg.MainButton.isVisible) {
+    if(tg.MainButton.isVisible && item == "6") {
         tg.MainButton.hide();
+        tg.MainButton.color = "#d4a066"; // Сброс цвета кнопки
+        item = "";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 6!")
         item = "6";
+        tg.MainButton.color = "#2cab37"; // Устанавливаем цвет кнопки
         tg.MainButton.show();
     }
 });
